831636e95c46ec04bd614e123b6e513b2e662724
george-iot <georgeshenoda@protonmail.com>
2025-11-20 17:21:09 +0200

    add header

---
+++ src/ts/main.ts
@@ -0,0 +1,12 @@
+let toggle_sidebar = document.querySelector("header .toggle-sidebar");
+let sidebar = document.querySelector(".sidebar") as HTMLDivElement
+let content_area = document.querySelector(".content-area") as HTMLDivElement
+
+if (toggle_sidebar) {
+    toggle_sidebar.addEventListener("click", () => {
+        if(sidebar && content_area){
+            sidebar.classList.toggle("no-sidebar")
+            content_area.classList.toggle("no-sidebar")
+        }
+    });
+}
