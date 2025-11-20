e3ede2fde5c3d6a139cd942a8ea25cb4207eb07c
george-iot <georgeshenoda@protonmail.com>
2025-11-20 23:51:16 +0200

    push header

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 let toggle_sidebar = document.querySelector("header .toggle-sidebar");
-let sidebar = document.querySelector(".sidebar") as HTMLDivElement
-let content_area = document.querySelector(".content-area") as HTMLDivElement
+let sidebar = document.querySelector(".sidebar") as HTMLDivElement;
+let content_area = document.querySelector(".content-area") as HTMLDivElement;
+let toggle_submenus = document.querySelectorAll(".toggle-submenu");
 
 if (toggle_sidebar) {
     toggle_sidebar.addEventListener("click", () => {
-        if(sidebar && content_area){
-            sidebar.classList.toggle("no-sidebar")
-            content_area.classList.toggle("no-sidebar")
+        if (sidebar && content_area) {
+            sidebar.classList.toggle("no-sidebar");
+            content_area.classList.toggle("no-sidebar");
         }
+    });
+}
+
+if (toggle_submenus) {
+    toggle_submenus.forEach((toggle_submenu) => {
+        toggle_submenu.addEventListener("click", () => {
+            toggle_submenu.children[0].classList.toggle("down");
+            toggle_submenu.nextElementSibling?.classList.toggle("show")
+        });
     });
 }
